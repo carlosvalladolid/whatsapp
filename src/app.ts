779f7786bced1765e162d59f613bca62686c65f3
Carlos Valladolid <lachoman@hotmail.com>
2024-05-09 15:51:13 -0600

    Agregué archivo de ecosystem.config.js

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,38 @@
 import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from '@bot-whatsapp/bot'
 import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys'
+import fetch from 'node-fetch';
 
-const flowBienvenida = addKeyword('hola').addAnswer('Buenas, buenas!')
+// Function to call a web API
+async function callWebApi(url: string, message: string) {
+    try {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message })  // Convert the message object to a JSON string
+        };
+
+        const response = await fetch(url, options);
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.error('Failed to fetch data from API:', error);
+        return null;
+    }
+}
+
+const flowBienvenida = addKeyword('hola')
+    .addAnswer('Buenas, buenas!')
+
+const flowCarbyne = addKeyword('911-USAMEX')
+    .addAction(async(ctx, { flowDynamic }) => {
+        const apiResponse = await callWebApi('https://kipcalm.azurewebsites.net/Whatsapp/getWhatsappMessage', ctx.body);
+
+        return flowDynamic(`Tu mensaje es: ${ctx.body }`)
+    })
 
 const main = async () => {
 
@@ -15,7 +46,7 @@ const main = async () => {
     }))
     
     await createBot({
-        flow: createFlow([flowBienvenida]),
+        flow: createFlow([flowBienvenida, flowCarbyne]),
         database: new MemoryDB(),
         provider: provider
     })
